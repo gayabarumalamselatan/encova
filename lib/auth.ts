8d0d054e93cd873bd9e2385ddc71ecc580c319ad
febri <soerjahartono22@gmail.com>
2026-07-22 15:55:41 +0700

    fix: adjust cookie setting

--- lib/auth.ts
+++ lib/auth.ts
@@ -32,7 +32,7 @@ export async function setToken(token: string) {
     name: COOKIE_NAME,
     value: token,
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: process.env.NEXT_COOKIE_SECURE === "true",
     sameSite: "lax",
     path: "/",
     maxAge: 60 * 60 * 24, // 24 hours
